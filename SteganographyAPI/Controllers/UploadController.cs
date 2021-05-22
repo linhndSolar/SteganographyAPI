0739221bab7933264fef25b8e47f43eae0691ddd
linhndSolar <linhnd1@solarapp.asia>
2021-05-22 10:49:50 +0700

    Upload and convert to BMP

--- SteganographyAPI/Controllers/UploadController.cs
+++ SteganographyAPI/Controllers/UploadController.cs
@@ -4,12 +4,16 @@ using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SteganographyAPI.Controllers
 {
+    [Route("api/[controller]")]
     public class UploadController : Controller
     {
         [HttpPost, DisableRequestSizeLimit]
@@ -17,19 +21,23 @@ namespace SteganographyAPI.Controllers
         {
             try
             {
+                Image image;
                 var file = Request.Form.Files[0];
                 var folderName = Path.Combine("Resources", "Images");
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
                 if (file.Length > 0)
                 {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    // var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var id = DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss");
+                    var fileName = id +".bmp";
                     var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(folderName, fileName);
-                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    if (System.IO.File.Exists(fullPath))
                     {
-                        file.CopyTo(stream);
+                        System.IO.File.Delete(fullPath);
                     }
-                    return Ok(new { dbPath });
+
+                    Save(file, fullPath);
+                    return Ok(new { id });
                 }
                 else
                 {
@@ -39,6 +47,22 @@ namespace SteganographyAPI.Controllers
             catch (Exception ex)
             {
                 return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        public static async void Save(IFormFile image, string pathImage)
+        {
+            using (var stream = image.OpenReadStream())
+            using (var imgIS = Image.Load(stream, out IImageFormat format))
+            using (var memoryStream = new MemoryStream())
+            using (var fileStream = new FileStream(pathImage, FileMode.OpenOrCreate))
+            {
+                imgIS.SaveAsBmp(memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                await memoryStream.CopyToAsync(fileStream).ConfigureAwait(false);
+                fileStream.Flush();
+                memoryStream.Close();
+                fileStream.Close();
             }
         }
     }
