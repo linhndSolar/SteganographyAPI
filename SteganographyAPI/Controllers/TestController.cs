f5755b0f74377d24de4ef597a360f9eb0ab2e21d
linhnd99 <linhcoi.1999@gmail.com>
2021-05-22 09:19:42 +0700

    Init project

---
+++ SteganographyAPI/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SteganographyAPI.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SteganographyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public TestModel Post([FromBody] TestModel value)
+        {
+            return value;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SteganographyAPI/Model/TestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SteganographyAPI.Model
+{
+    public class TestModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+
+        public TestModel()
+        {
+
+        }
+    }
+}
